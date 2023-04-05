7922efb4a368a9dd0593ce9e2cb737e8e4ac226e
ling xiao <lingxiao3218@gmail.com>
2023-04-05 11:06:41 +0800

    validate external ip provided

--- refresh_wireguard_ip.sh
+++ refresh_wireguard_ip.sh
@@ -12,6 +12,13 @@ source /etc/pivpn/wireguard/setupVars.conf
 # Get public IP address
 public_ip=$(curl -s https://api.ipify.org)
 
+if ! [[ $public_ip =~ ^[0-9]+.[0-9]+.[0-9]+.[0-9]+$ ]]; then
+echo "Invalid IP address: $public_ip"
+exit 1
+fi
+
+echo "Valid ip $public_ip , proceed"
+
 # Check if pivpnHOST needs updating
 if [ "$public_ip" != "$pivpnHOST" ]; then
   # Update pivpnHOST in config file
@@ -19,7 +26,7 @@ if [ "$public_ip" != "$pivpnHOST" ]; then
   echo "pivpnHOST updated to $public_ip"
 
   # Send new IP address to Discord webhook
-  DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/"
+  DISCORD_WEBHOOK_URL="https://discord.com/"
   DISCORD_MESSAGE="New public IP address: $public_ip"
   curl -H "Content-Type: application/json" -d "{\"content\": \"$DISCORD_MESSAGE\"}" "$DISCORD_WEBHOOK_URL"
 else
